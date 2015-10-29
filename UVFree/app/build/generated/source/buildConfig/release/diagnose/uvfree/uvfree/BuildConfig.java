4368fa062c2ab900052c5b8ede05ebe434e83a7f
shehryarsiddiqui <sms.vln@gmail.com>
2015-10-29 16:18:59 -0500

    Image Crash Fixes
    Reduced minimum image sizes in calls to decodeSampledBitmapFromFile
    Still hardcoded - fix eventually
    Added bitmap sampling to PreviewDiag.java
    
    Next steps: find memory leaks?

---
+++ UVFree/app/build/generated/source/buildConfig/release/diagnose/uvfree/uvfree/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package diagnose.uvfree.uvfree;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "diagnose.uvfree.uvfree";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
